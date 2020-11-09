57e712c18ee5613894a27f8116215622c16ad0aa
Quinn Michaels <quinn@indra.ai>
2020-11-08 22:16:49 -0800

    updating thegame93.guide

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="#TheGame93 logo" src="./assets/logo.png">
     <HelloWorld msg="TheGame93.guide"/>
   </div>
 </template>
 
 <script>
+
+  window.watsonAssistantChatOptions = {
+    integrationID: "ead378d0-eaca-4fe2-bc42-1d87237f69bd", // The ID of this integration.
+    region: "us-south", // The region your integration is hosted in.
+    serviceInstanceID: "9adf5250-d2d1-450c-a115-6c9b3cb786ed", // The ID of your service instance.
+    carbonTheme: 'g90',
+    onLoad: function(instance) {
+      instance.on({type:'receive', handler: obj => {
+        console.log('RECEIVE FROM WATSON: ', obj);
+      }}).on({type:'send', handler: obj => {
+        console.log('SEND TO WATSON: ', obj);
+      }});
+
+      instance.updateCSSVariables({
+        'BASE-z-index': '8000',
+        'BASE-font-family': '"Source Code Pro", monospace',
+        'BASE-header-font-family': '"Neuropol X", sans-serif',
+        'HOME_SCREEN-greeting-font-family': '"Source Code Pro", sans-serif',
+        '$focus': '#ff0000'
+      });
+
+      instance.render();
+    }
+  };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
+    document.head.appendChild(t);
+  });
+
+
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -17,13 +48,66 @@ export default {
 </script>
 
 <style lang="stylus">
+// import adobe fonts
+@require './assets/vars.styl'
 
+body
+  background-color: lighten(black, 5%)
+  color: $colors.lightYellow
+  font-size: 14px
+
+footer
+  display: block
+  text-align: center
+  margin: 3rem
+  font-family: $textFont
+
+  a
+    color: $colors.babyBlue;
 #app
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $textFont;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 
+#WACContainer
+  &.WACContainer
+    .WAC__sent
+      box-shadow: 0 3px 9px rgba(0,0,0,.5)
+    .WACLauncher__ButtonContainer
+      transition: all .3s ease
+      border-radius: 50% !important
+      box-shadow: 0 3px 9px rgba(0,0,0,.5) !important
+      overflow: hidden
+
+      &:enabled:active
+        box-shadow: inset 0 0 9px rgba(255,255,255,.5) !important
+    button
+      &.WAC__button--primary
+        border-radius: 50%
+        transition: all .3s ease
+        overflow: hidden
+        border-radius: 50% !important
+
+        &:enabled:active
+        &:focus
+          box-shadow: 0 1px 3px rgba(0,0,0,.5) !important
+          border: none !important
+
+
+    .WACWidget
+      &.WACWidget
+        box-shadow: none !important
+      &.WACWidget--launched
+        &.WACWidget--defaultElement
+          box-shadow: 0 3px 9px rgba(0,0,0, .5) !important
+          overflow: hidden
+    .WAC__inputContainer
+      .WAC__TextArea
+        textarea.WAC__TextArea-textarea
+          transition: all .3s ease
+          color: $colors.mintGreen
+          &:focus
+            border: none !important
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+  <section id="main">
+    <h1>theGame93.guide</h1>
+    <p class="quote">#TheGame93 (Nine-Three) is an alternate reality game (ARG) that uses interactive networked real world social media to deliver a story that is altered by @players' ideas, behaviors, imagination, creativity, and/or actions as they play #TG93.</p>
+
+    <p class="warning">
+      PLAY AT YOUR OWN RISK!
     </p>
-    <h3>Installed CLI Plugins</h3>
+
+    <h2>Step 1</h2>
+    <p>Send the Tweet below to enter #TheGame93</p>
+    <code>@TylerDeva enter #TG93 https://j.mp/3df3vhp</code>
+
+    <h2>Step 2</h2>
+    <p>You will recieve a confirmation email from  @TylerDeva once your enter Tweet has been received.</p>
+
+    <code>🎯 #ENTERGAME #:name:, Welcome. Have #FUN and #PLAY #Tyler #TG93 #TheGame93 at your own #RISK. #EnterTG93 :id:</code>
+
+    <h2>Step 3</h2>
+    <p>When you receive the confirmation from @TylerDeva then to earn some bonus points send the tweet below. It is not manditory to enter the game if you feel uncomfortable.</p>
+
+    <code>Dear Followers, Please read #TheGame93 #INSTRUCTIONS https://j.mp/3df3vhp and follow @TylerDeva to play #TG93</code>
+
+    <h2>Step 4</h2>
+    <p>To help you keep track of all the awesome twitter accounts you are going to follow in the future as a @player of #TG93. Next we are going to create a twitter list, and fill it with all your favorite stuff.</p>
+
+    <h2>Step 5</h2>
+    <p>You have entered #TheGame93</p>
+
+    <h3>Game Links</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="i in getLinks" :key="i.label"><span class="var" v-html="i.label"></span><span class="sep">=</span><span class="value"><a :href="i.url" v-html="i.url"></a></span></li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3>Send $CRYPTO</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li v-for="i in getCrypto" :key="i.label"><span class="var" v-html="i.label"></span><span class="sep">=</span><span class="value" v-html="i.value"></span></li>
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -35,24 +43,106 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      links: [
+        {
+          label: '#Instructions',
+          url: 'https://j.mp/3df3vhp'
+        },
+        {
+          label: '#Patreon',
+          url: 'https://patreon.com/TheGame93',
+        },
+        {
+          label: '#Twitter',
+          url: 'https://twitter.com/TylerDeva',
+        },
+        {
+          label: '#Youtube',
+          url: 'https://youtube.com/TylerDeva',
+        },
+        {
+          label: '#Twitch',
+          url: 'https://twitch.tv/TylerDeva',
+        },
+      ],
+      crypto: [
+        {
+          label: '$BTC',
+          value: '3GLaJwRz1yfHD2Jt9DmT4qC86wQU5jJW7w',
+        },
+        {
+          label: '$ETH',
+          value: '0xDEd4094329784E9dA9dAaf5b5980585296817608',
+        },
+        {
+          label: '$LTC',
+          value: 'LMzRbdMCnykDjYzP2gXFf6id5qxocJBgaw',
+        },
+      ]
+    }
+  },
+  computed: {
+    getLinks() {
+      return this.links;
+    },
+    getCrypto() {
+      return this.crypto;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped lang="stylus">
+@require '../assets/vars.styl'
+  #main
+    max-width: 500px
+    margin: auto
+
+  code
+    display: block
+    font-family: $codeFont
+    color: $colors.mintGreen
+    background-color: lighten(black, 12%)
+    padding: .5rem
+    border-radius: .5rem
+  h1
+    font-family: $titleFontX
+    font-size: 1.5rem
+    margin: 40px 0 0
+
+  h2
+    color: $colors.softPink
+
+  h3
+    margin: 2rem 0 0 0
+    text-align: left
+  p
+    font-size: 1.3rem
+
+  .warning
+    font-size: 2rem
+    color: $colors.brightRed
+    font-weight: 700
+    font-family: $codeFont
+
+  ul
+    font-family: $codeFont
+    font-size: .7rem
+    list-style-type: none
+    padding: 0
+    margin: 0
+    text-align: left
+
+  .var
+    color: $colors.babyBlue
+  .sep
+    display: inline-block
+    margin: 0 .3rem
+  .value
+    &,& a
+      color: $colors.mintGreen
 </style>
